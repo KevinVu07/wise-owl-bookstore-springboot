eb821f24ff1ecd99b1a8b3836a32ad030d0105aa
Kevin Vu <hongkhanhvu94@gmail.com>
2025-04-28 15:47:18 +1000

    convert book-details jsp file to Thymeleaf html file. Fix some styling and make all pages responsive

--- src/main/java/com/wiseowl/bookstore/controller/BookController.java
+++ src/main/java/com/wiseowl/bookstore/controller/BookController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -20,5 +21,16 @@ public class BookController {
         List<Book> books = bookRepository.findAll();
         model.addAttribute("books", books);
         return "book-list";
+    }
+
+    @GetMapping("/book/{id}")
+    public String showBookDetails(@PathVariable("id") int id, Model model) {
+        Book book = bookRepository.findById(id).orElse(null);
+        if (book != null) {
+            model.addAttribute("book", book);
+            return "book-details";
+        } else {
+            return "redirect:/books"; // Redirect to the list if the book is not found
+        }
     }
 }
